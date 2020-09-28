4fb5cb5c71cd93a01557cc9cebb9ce64d84cf955
premchandAllu <premchandallu1999@gmail.com>
2020-09-28 09:09:48 +0530

    Added: Welcome Message is added

---
+++ AddressBook.java
@@ -0,0 +1,12 @@
+package com.Bridgelabz.AddressBook;
+
+public class AddressBook 
+{
+  public static void main(String args[])
+    { 
+	   //Printing Output 
+	   System.out.println("Welcome to Address Book Program");
+    }
+ 
+
+}
