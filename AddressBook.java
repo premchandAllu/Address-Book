85c2bd54c845c626937a473038e40f89915205f3
premchandAllu <premchandallu1999@gmail.com>
2020-09-28 14:02:32 +0530

    Added: details for another person are alos added

--- AddressBook.java
+++ AddressBook.java
@@ -1,21 +1,20 @@
 package com.Bridgelabz.AddressBook;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBook 
 {
-	private String firstName;
-	private String lastName;
-	private String address;
-	private String city;
-	private String state;
-	private int zip;
-	private long phoneNumber;
-	private String emailId; 
-	
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+	long phoneNumber;
+	String emailId; 
+	  
 	
 public void Address(String firstName, String lastName, String address, String city, String state,int zip,long phoneNumber, String emailId)
     {
-	
      	this.firstName=firstName;
 		this.lastName=lastName;
 		this.address=address;
@@ -26,38 +25,47 @@ public void Address(String firstName, String lastName, String address, String ci
 		this.emailId=emailId;
 		
 		System.out.println("Address Book Details of the person are");
-		System.out.println( firstName+"\t"+lastName+"\t"+address+"\t"+city+"\t"+state+"\t"+zip+"\t"+phoneNumber+" "+emailId);
-		
+		System.out.println( firstName+" "+lastName+" "+address+" "+city+" "+state+" "+zip+" "+phoneNumber+" "+emailId+" ");
 		
 		}
+
   public static void main(String args[])
     { 
-	  Scanner sc = new Scanner(System.in);
+	  char choice;
+    
+	  do
+	  {
+		  Scanner sc = new Scanner(System.in);
 	  System.out.println("Add details in Address Book");
-	  
+	
 	  //taking input from user
 	  System.out.println("Enter the first name");
-	  String firstName =sc.nextLine( );
+	  String firstName = sc.nextLine( );
 	  System.out.println("Enter the last name");
-	  String lastName =sc.nextLine( );
+	  String lastName = sc.nextLine( );
 	  System.out.println("Enter the address");
-	  String address  =sc.nextLine( );
+	  String address  = sc.nextLine( );
 	  System.out.println("Enter the city");
-	  String city     =sc.nextLine( );
+	  String city     = sc.nextLine( );
 	  System.out.println("Enter the state");
-	  String state    =sc.nextLine( );
+	  String state    = sc.nextLine( );
 	  System.out.println("Enter the zip number");
-	  int zip         =sc.nextInt( );
+	  int zip         = sc.nextInt( );
 	  System.out.println("Enter the phone number");
-	  long phoneNumber =sc.nextLong( );
+	  long phoneNumber= sc.nextLong( );
 	  System.out.println("Enter the email id");
-	  String emailId = sc.nextLine( );
+	  sc.nextLine();
+	  String emailId  = sc.nextLine( );
 	  
 	  AddressBook obj = new AddressBook();
 	  
 	  obj.Address(firstName,lastName,address,city,state,zip,phoneNumber,emailId);
+	  
+	  System.out.println("do you want to enter detials for another person, if yes type y  else type n ");
+	  choice= sc.next().charAt(0);
+	  
 	    
-    }
- 
-
+    }while( choice == 'y'|| choice =='Y');
+    	
+  }
 }
